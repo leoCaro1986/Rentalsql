cafde6cac17a09fc9c17e6059dd91da527f07125
Leonardo Caro <leocv0205@gmail.com>
2021-04-17 23:34:34 -0500

    Conexion a base de datos

--- index.py
+++ index.py
@@ -1,4 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector 
+import mysql
+
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    port = 3306,
+    password = "",
+    database = "rental_db"
+)
+myCursor = mydb.cursor()
+
+ 
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,9 +29,55 @@ def login():
 def sign_in():
     return render_template("sign_in.html")
 
+@app.route('/sign_up',methods=["POST"])
+def sign_up():
+    if request.method == "POST":
+        name = request.form['name']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        country = request.form['country']
+        city = request.form['city']
+        password = request.form['password']
+        roll = request.form['roll']
+        query = f"INSERT INTO user (name, lastname, email, country, city, password, roll) VALUES('{name}','{lastname}', '{email}','{country}','{city}','{password}','{roll}')"
+        myCursor.execute(query)
+        mydb.commit()
+        return redirect(url_for('index'))
+    else:
+        return "Bad request"
+
+
+@app.route('/read')
+def read():
+    query = "SELECT * FROM user"
+    myCursor.execute(query)
+    result = myCursor.fetchall()
+    print(result)
+    return render_template("sign_in")
+
+
 @app.route('/addProperty')
 def addProperty():
     return render_template("addProperty.html")    
+
+@app.route('/property',methods=["POST"])
+def property():
+    if request.method == "POST":
+        cityP = request.form['cityP']
+        countryP = request.form['countryP']
+        adressP = request.form['adressP']
+        ubication = request.form['ubication']
+        roomNumber = request.form['roomNumber']
+        imageP = request.form['imageP']
+        priceDay = request.form['priceDay']
+        Description = request.form['Description']
+        query = f"INSERT INTO property (cityP, countryP, adressP, ubication, roomNumber, imageP, priceDay, Description ) VALUES('{cityP}','{countryP}','{adressP}','{ubication}',{roomNumber},'{imageP}',{priceDay},'{Description}')"
+        myCursor.execute(query)
+        mydb.commit()
+        return redirect(url_for('index'))
+    else:
+        return "Bad request"
+ 
 
 if __name__ == '__main__':
     app.run(debug=True) #se genera el servidor
--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 
 //Validacion del login
 function validateLogin() {
-    debugger;
+  
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     // const pattern = new RegExp('^[A-Z]+$', 'i');  /^(?=.\d)(?=.[A-Za-z])[0-9A-Za-z¡@#$%&?¿!]{8,16}$/
@@ -13,10 +13,7 @@ function validateLogin() {
       alert('El campo email es necesario, por favor verifiquelo');
       return false;
     }
-    // if (!pattern.test(username)){
-    //    alert('digite solo letras'); 
-    //    return false;
-    // }
+    
     
     if(password =="") {
         alert('El campo password es requerido');
@@ -35,13 +32,14 @@ function validateLogin() {
 
 //Validacione del registro sign_in
 function validasign_in(){
+   
     let name = document.getElementById("name").value;
     let lastname = document.getElementById("lastname").value;
     let email = document.getElementById("email").value;
     let country = document.getElementById("country").value;
     let city = document.getElementById("city").value;
     let password = document.getElementById("password").value;
-    let tipeCliente = document.getElementById("tipeCliente").value;
+    let roll = document.getElementById("roll").selectedIndex;
     const pattern = new RegExp('^[A-Z]+$', 'i');
     const vemail = new RegExp(/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/);
     const vpassword = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,14}$/);
@@ -70,11 +68,12 @@ function validasign_in(){
         alert('Verifique que el campo email sea correcto'); 
         return false;
      }
-     // value city
+     // value country
      if (!pattern.test(country)){
         alert('Verifique el campo Pais'); 
         return false;
      }
+     // value city
      if (!pattern.test(city)){
         alert('Verifique el campo Ciudad'); 
         return false; 
@@ -84,39 +83,85 @@ function validasign_in(){
         alert('El campo password no tiene los caracteres requeridos'); 
         return false;
      }
+    
+     if (roll == null || roll == 0 ){
+      alert('Por favor selecciona tu rol'); 
+      return false;
+     }
+     
+   
+     
 }
 
 
 //Validacion addProperty
 function validaaddProperty(){
-   let cityP = document.getElementById(cityP).value;
-   let countryP = document.getElementById(countryPP).value;
-   let adressP = document.getElementById(adressP).value;
-   let ubication = document.getElementById(ubication).value;
-   let room_Number = document.getElementById(room_Number).value;
-   let image_P = document.getElementById(image_P).value;
-   let imageMain = document.getElementById(imageMain).value;
-   let valueDay = document.getElementById(valueDay).value;
-   let Description = document.getElementById(Description).value;
-   const pattern = new RegExp('^[A-Z]+$', 'i');
-   const number = new RegExp('^[0-9]+$', 'i');
 
-   if (!pattern.test(cityP)){
-      alert('El campo ciudad debe contener solo texto'); 
+   let cityP = document.getElementById("cityP").value;
+   let countryP = document.getElementById("countryP").value;
+   let adressP = document.getElementById("adressP").value;
+   let ubicationP = document.getElementById("ubication").value;
+   let roomNumber = document.getElementById("roomNumber").value;
+   let imageP = document.getElementById("imageP").value;
+   let imageMain = document.getElementById("imageMain").value;
+   let priceDay = document.getElementById("priceDay").value;
+   let Description = document.getElementById("Description").value;
+   const pattern = new RegExp('^[A-Z]+$', 'i');
+   const url = new RegExp(/^http\:\/\/|https\:\/\/|www\.google$/);
+debugger; 
+
+
+//Validacion de ciudad
+   if (cityP.length == 0  || cityP == "") {
+      alert('Digite el campo Ciudad');
       return false;
-   }
-   if (!pattern.test(countryP)){
-      alert('El campo país debe contener solo texto'); 
+    }
+    if (!pattern.test(cityP)){
+      alert('El campo Ciudad solo debe contener texto'); 
       return false;
-   }
-   if (adressP.length == 0  || adressP== "") {
-      alert('Digite la dirección');
+    }
+//Validacion de pais
+    if (countryP.length == 0  || countryP == "") {
+       alert('Digite el campo Pais');
+       return false;
+    }
+    if (!pattern.test(countryP)){
+       alert('El campo Pais solo debe contener texto'); 
+       return false;
+    }
+//Validacion de la dirección
+    if (adressP.length == 0  || adressP == "") {
+       alert('El campo Dirección debe estar diligenciado');
+       return false;
+    }
+
+    if (ubicationP == null  || ubicationP == "") {
+      alert('Verifique ubicación');
       return false;
-   }
-   if (!number.test(room_Number)){
-      alert('numero de habitación no valido'); 
+    } 
+    if (!url.test(ubicationP)){
+      alert('Verifique el campo ubicación'); 
       return false;
-   }
+    }
+      //Validacion de numero de habitaciones
+    if (roomNumber == 0  || roomNumber == "") {
+       alert('Debe diligenciar el numero de habitaciones');
+       return false;
+    } 
+   //  if (isNaN(room_Number)) {
+   //     alert('Digite solo números');
+   //     return false;
+   //  }
+    
+    if (priceDay == 0 || priceDay == ""){
+       alert('Digite Valor por noche');
+       return false;
+    }
+    if (Description.length == 0  || Description == "") {
+      alert('Digite la descripción de la propiedad');
+      return false;
+    }
+      
 
 
 }
